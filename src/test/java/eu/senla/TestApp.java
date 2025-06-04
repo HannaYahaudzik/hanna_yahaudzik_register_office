6224186c3627ec7e28a61a1cf496b89409fd998c
hanna.yahaudzik <hanna.yahaudzik@gmail.com>
2025-06-04 08:11:39 +0300

    Init project. Connect a checkstyle and TestNG

---
+++ src/test/java/eu/senla/TestApp.java
@@ -0,0 +1,13 @@
+package eu.senla;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class TestApp {
+
+    @Test
+    public void testFirst() {
+        Assert.assertTrue(true);
+    }
+}
