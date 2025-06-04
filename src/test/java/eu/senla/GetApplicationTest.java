1b63ea7cac8df8e16394081eaaf4728ae659efc0
hanna.yahaudzik <hanna.yahaudzik@gmail.com>
2025-06-04 16:24:24 +0300

    Processing a getApplication response. Connect Jackson.

--- src/test/java/eu/senla/GetApplicationTest.java
+++ src/test/java/eu/senla/GetApplicationTest.java
@@ -2,6 +2,8 @@ package eu.senla;
 
 
 import eu.senla.config.SpecConfig;
+import eu.senla.templates.getApplication.GetApplicationResponse;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import static io.restassured.RestAssured.given;
@@ -10,13 +12,31 @@ public class GetApplicationTest {
 
     @Test
     public void testFirst() {
+        GetApplicationResponse response =
+                given()
+                        .spec(SpecConfig.requestSpecification())
+                        .basePath("getApplications")
+                        .when()
+                        .get()
+                        .then()
+                        .spec(SpecConfig.responseSpecification())
+                        .extract().as(GetApplicationResponse.class);
 
-        given()
-                .spec(SpecConfig.requestSpecification())
-                .basePath("getApplications")
-                .when()
-                .get()
-                .then()
-                .spec(SpecConfig.responseSpecification());
+        Assert.assertNotNull(response.getTotal());
+    }
+
+    @Test
+    public void testSecond() {
+        GetApplicationResponse response =
+                given()
+                        .spec(SpecConfig.requestSpecification())
+                        .basePath("getApplications")
+                        .when()
+                        .get()
+                        .then()
+                        .spec(SpecConfig.responseSpecification())
+                        .extract().as(GetApplicationResponse.class);
+
+        Assert.assertNotNull(response.getData());
     }
 }
