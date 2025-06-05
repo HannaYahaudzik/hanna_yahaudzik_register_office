8088bd84876df19a481a506987b805ca4bdfa00e
hanna.yahaudzik <hanna.yahaudzik@gmail.com>
2025-06-05 09:27:17 +0300

    Fix checkstyle error

--- src/test/java/eu/senla/GetApplStatusTest.java
+++ src/test/java/eu/senla/GetApplStatusTest.java
@@ -5,9 +5,12 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GetApplStatusTest {
+
+    private final Integer APPLICATION_ID = 49998;
+
     @Test
-    public void schemaValidation(){
-        String requestId = new GetApplStatus(49998)
+    public void schemaValidation() {
+        String requestId = new GetApplStatus(APPLICATION_ID)
                 .getResponse()
                 .getRequestId();
         Assert.assertNotNull(requestId);
